121596fbefaa5adda76f66a19db854ddae18aa28
Marun0402 <110802170+Marun0402@users.noreply.github.com>
2023-05-16 08:23:23 +0530

    Add files via upload
    Sentient.io Rewamp desktop view code uploaded

---
+++ script.js
@@ -0,0 +1,27 @@
+let tabsContainer = document.querySelector("#tabs");
+
+let tabTogglers = tabsContainer.querySelectorAll("#tabs a");
+
+console.log(tabTogglers);
+
+tabTogglers.forEach(function(toggler) {
+  toggler.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    let tabName = this.getAttribute("href");
+
+    let tabContents = document.querySelector("#tab-contents");
+
+    for (let i = 0; i < tabContents.children.length; i++) {
+      
+      tabTogglers[i].parentElement.classList.remove("border-t-4","text-gray-500","border-t-gray-500","-mb-px");  
+      tabContents.children[i].classList.remove("hidden");
+      if ("#" + tabContents.children[i].id === tabName) {
+        continue;
+      }
+      tabContents.children[i].classList.add("hidden");
+      
+    }
+    e.target.parentElement.classList.add("border-t-4","border-t-lime-500","text-lime-500","-mb-px");
+  });
+});
